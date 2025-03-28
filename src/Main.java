2d15fdbebef8e372fb958d0287ed91579b0b53e5
Roncelli <leonardoroncelli@gmail.com>
2025-03-28 15:42:51 +0100

    Prima funzione.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<String>file=new ArrayList<String>();
+        Random random=new Random();
+        try(BufferedReader br=new BufferedReader(new FileReader("roncelli.csv"))){
+            String linea=br.readLine();
+            if(linea!=null) {
+                String intestazione = linea + ";MioValore;Cancellazione";
+                file.add(intestazione);
+            }
+            while (linea!=null){
+                linea= br.readLine();
+                int num= random.nextInt(11)+10;
+                file.add(linea+";"+num+";0");
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        try(BufferedWriter bw=new BufferedWriter(new FileWriter("roncelli.csv"))){
+            for(int i=0;i< file.size();i++){
+                bw.write(file.get(i));
+                bw.newLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
