1bb621280d2cdca1b6c5b9c88193ed1c69e64586
Roncelli <leonardoroncelli@gmail.com>
2025-03-30 15:18:23 +0200

    Funzione 4, al momento dell'esecuzione saranno aggiunti ad ogni record degli zeri (in modo da vedere la differenza di lunghezza) portando le righe tutte alla stessa dimensione.

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,52 @@ import java.util.List;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String>file=new ArrayList<String>();
-        Random random=new Random();
-        try(BufferedReader br=new BufferedReader(new FileReader("roncelli.csv"))){
-            String linea=br.readLine();
-            if(linea!=null) {
+        ArrayList<String> file = new ArrayList<String>();
+        Random random = new Random();
+        try (BufferedReader br = new BufferedReader(new FileReader("roncelli.csv"))) {
+            String linea = br.readLine();
+            if (linea != null) {
                 String intestazione = linea + ";MioValore;Cancellazione";
                 file.add(intestazione);
             }
-            while (linea!=null){
-                linea= br.readLine();
-                int num= random.nextInt(11)+10;
-                file.add(linea+";"+num+";0");
+            while (linea != null) {
+                linea = br.readLine();
+                int num = random.nextInt(11) + 10;
+                file.add(linea + ";" + num + ";0");
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("roncelli.csv"))) {
+            for (int i = 0; i < file.size(); i++) {
+                bw.write(file.get(i));
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] file2 = file.get(0).split(";");
+        System.out.println("Numero di campi: " + file2.length);
+        int max = file.get(0).length();
+        int indice = 0;
+        for (int i = 1; i < file.size(); i++) {
+            if (file.get(i).length() > max) {
+                max = file.get(i).length();
+                indice = i + 1;
+            }
+        }
+        System.out.println("Massimo: " + max);
+        System.out.println("Riga che contiene il recordo più lungo: " + indice);
+        System.out.println(file.get(indice - 1));
+        for(int i=0;i< file.size();i++){
+            String riga=file.get(i);
+            if(riga.length()<max){
+                int diff=max-riga.length();
+                for(int j=0;j<diff;j++){
+                    riga=riga+"0";
+                }
+                file.set(i,riga);
+            }
         }
         try(BufferedWriter bw=new BufferedWriter(new FileWriter("roncelli.csv"))){
             for(int i=0;i< file.size();i++){
@@ -28,17 +59,5 @@ public class Main {
         }catch (IOException e){
             e.printStackTrace();
         }
-        String[]file2=file.get(0).split(";");
-        System.out.println("Numero di campi: "+file2.length);
-        int max=file.get(0).length();
-        int indice=0;
-        for(int i=1;i< file.size();i++){
-            if(file.get(i).length()>max){
-                max=file.get(i).length();
-                indice=i+1;
-            }
-        }
-        System.out.println("Massimo: "+max);
-        System.out.println("Riga che contiene il recordo più lungo: "+indice);
     }
 }
